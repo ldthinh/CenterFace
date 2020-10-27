902680efbcf094e48af5cb17dc4bb585a72c5e5b
Thinh Le <44941261+ldthinh@users.noreply.github.com>
2020-10-27 14:36:03 +0700

    Add files via upload

---
+++ prj-tensorrt/convert_trt.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import datetime
+import pycuda.autoinit
+import pycuda.driver as cuda
+import tensorrt as trt
+import os
+
+def get_engine(max_batch_size=1, onnx_file_path="", engine_file_path="", fp16_mode=False, int8_mode=False, save_engine=False):
+    """Attempts to load a serialized engine if available, otherwise builds a new TensorRT engine and saves it."""
+    TRT_LOGGER = trt.Logger()
+    def build_engine(max_batch_size, save_engine):
+        """Takes an ONNX file and creates a TensorRT engine to run inference with"""
+        with trt.Builder(TRT_LOGGER) as builder, \
+                builder.create_network() as network, \
+                trt.OnnxParser(network, TRT_LOGGER) as parser:
+
+            builder.max_workspace_size = 1 << 30  # Your workspace size
+            builder.max_batch_size = max_batch_size
+            # pdb.set_trace()
+            builder.fp16_mode = fp16_mode  # Default: False
+            builder.int8_mode = int8_mode  # Default: False
+            if int8_mode:
+                # To be updated
+                raise NotImplementedError
+
+            # Parse model file
+            if not os.path.exists(onnx_file_path):
+                quit('ONNX file {} not found'.format(onnx_file_path))
+
+            print('Loading ONNX file from path {}...'.format(onnx_file_path))
+            with open(onnx_file_path, 'rb') as model:
+                print('Beginning ONNX file parsing')
+                parser.parse(model.read())
+
+            print('Completed parsing of ONNX file')
+            print('Building an engine from file {}; this may take a while...'.format(onnx_file_path))
+
+            engine = builder.build_cuda_engine(network)
+            print("Completed creating Engine")
+
+            if save_engine:
+                with open(engine_file_path, "wb") as f:
+                    f.write(engine.serialize())
+            return engine
+
+    if os.path.exists(engine_file_path):
+        # If a serialized engine exists, load it instead of building a new one.
+        print("Reading engine from file {}".format(engine_file_path))
+        with open(engine_file_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
+            return runtime.deserialize_cuda_engine(f.read())
+    else:
+        return build_engine(max_batch_size, save_engine)
+
+if __name__ == '__main__':
+    get_engine(max_batch_size=1, onnx_file_path="model_onnx/centerface_480_640.onnx", engine_file_path="model_trt/centerface.trt", fp16_mode=False, int8_mode=False, save_engine=True)
